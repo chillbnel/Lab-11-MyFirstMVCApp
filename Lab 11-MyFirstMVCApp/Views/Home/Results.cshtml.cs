ec49e46c899454311218bc7b20eb9414fe1541c4
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-23 12:19:39 -0700

    Added cvs file and get/post to the controller

--- Lab 11-MyFirstMVCApp/Views/Home/Results.cshtml.cs
+++ Lab 11-MyFirstMVCApp/Views/Home/Results.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Lab 11-MyFirstMVCApp.Views.Home
+namespace Lab11MyFirstMVCApp.Views.Home
 {
     public class ResultsModel : PageModel
     {
